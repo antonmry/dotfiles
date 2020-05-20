a7ac92da7b84b0011b39d4af4cad08fac3d079d9
antonmry <anton@galiglobal.com>
2020-05-20 17:11:17 +0200

    Update bin/executable_tmux.sh

--- bin/executable_tmux.sh
+++ bin/executable_tmux.sh
@@ -6,6 +6,8 @@ exec alacritty -e tmux new-session \
 "gcalcli agenda --calendar 'Anton Rodríguez Yuste' --military 00:00 23:59; read" \;  \
 split-window "curl wttr.in/ponteceso; read" \; \
 resize-pane -y 40 \; \
+select-pane -t:.2 -P 'fg=white,bg=black' \; \
 select-pane -t:.1 \; \
-split-window -h "cowsay 'Bos días!!'; read" \; \
+split-window -h "todo.sh ls; read" \; \
 new-window\; next-window
+#split-window -h "cowsay 'Bos días!!'; read" \; \
