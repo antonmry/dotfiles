626015e3f7cb070d14af70e575d6c3fece134660
antonmry <anton@galiglobal.com>
2020-06-23 07:51:01 +0200

    Update bin/executable_tmux.sh

--- bin/executable_tmux.sh
+++ bin/executable_tmux.sh
@@ -3,7 +3,7 @@
 #exec gnome-terminal -e tmux
 
 exec alacritty -e tmux new-session \
-"gcalcli agenda --calendar 'Anton Rodríguez Yuste' --military 00:00 23:59; read" \;  \
+"gcalcli --calendar 'Anton Rodríguez Yuste' agenda 00:00 23:59; read" \;  \
 split-window "curl wttr.in/ponteceso; read" \; \
 resize-pane -y 40 \; \
 select-pane -t:.2 -P 'fg=white,bg=black' \; \
