e31a454c9220a5098d0280ac0c8c9952cbb57750
antonmry <antonmry@inditex.com>
2020-12-01 07:55:24 +0100

    Update bin/executable_tmux.sh

--- bin/executable_tmux.sh
+++ bin/executable_tmux.sh
@@ -10,6 +10,6 @@ select-pane -t:.2 -P 'fg=white,bg=black' \; \
 select-pane -t:.1 \; \
 split-window -h "todo ls | tac; read" \; \
 select-pane -t:.1 \; \
-# split-window "rsstail -r https://www.upwork.com/ab/feed/topics/rss?securityToken=`gopass websites/upwork/rss-token`&userUid=424214442304323584&orgUid=424214442308517889 | tac; read" \; \
 split-window "cowsay 'Bos días!!'; read" \; \
 new-window \; next-window \; \
+# split-window "rsstail -r https://www.upwork.com/ab/feed/topics/rss?securityToken=`gopass websites/upwork/rss-token`&userUid=424214442304323584&orgUid=424214442308517889 | tac; read" \; \
