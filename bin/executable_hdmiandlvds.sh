6d6504a5474c838d95faec9e208497c242df0a8b
antonmry <antonmry@inditex.com>
2020-12-03 21:30:42 +0100

    Update bin/executable_hdmiandlvds.sh

--- bin/executable_hdmiandlvds.sh
+++ bin/executable_hdmiandlvds.sh
@@ -1,8 +1,18 @@
 #! /bin/bash
-sed -i -e 's/exec xrandr --dpi 200/exec xrandr --dpi 100/g' /home/antonmry/.config/i3/config
 
-sed -i -e 's/Xft.dpi:       220/Xft.dpi:       100/g' /home/antonmry/.Xresources
+case $HOSTNAME in
+  (gali8)
+    sed -i -e 's/exec xrandr --dpi 200/exec xrandr --dpi 100/g' /home/antonmry/.config/i3/config
+    sed -i -e 's/Xft.dpi:       220/Xft.dpi:       100/g' /home/antonmry/.Xresources
+    xrandr --output HDMI-1 --auto --primary --output eDP-1 --auto
+    xrdb ~/.Xresources
+    i3-msg restart
+      ;;
+  (gali9)
+    xrandr --output HDMI-1 --auto --primary --output eDP-1 --auto
+    xrdb ~/.Xresources
+    i3-msg restart
+      ;;
+  (*)   echo "How did I get in the middle of ${HOSTNAME}?";;
+esac
 
-xrandr --output HDMI-1 --auto --primary --output eDP-1 --auto
-xrdb ~/.Xresources
-i3-msg restart
