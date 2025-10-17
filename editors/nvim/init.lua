33d86f16398a485e1884c4d357b9fdd825c1c2b2
Anton Rodriguez <anton@galiglobal.com>
2025-10-17 19:41:04 +0200

    Minor changes

--- editors/nvim/init.lua
+++ editors/nvim/init.lua
@@ -207,20 +207,15 @@ vim.keymap.set("n", "<leader>:", function()
 	vim.cmd("lua require('fzf-lua').commands()")
 end, { silent = true })
 
+-- See the doc for the global defaults: https://neovim.io/doc/user/lsp.html
 vim.api.nvim_create_autocmd("LspAttach", {
 	desc = "LSP actions",
 	callback = function(event)
 		local opts = { buffer = event.buf }
-		-- vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
-		vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
-		vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
-		vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
-		vim.keymap.set("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
-		vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
-		vim.keymap.set("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
-		vim.keymap.set("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
-		vim.keymap.set({ "n", "x" }, "<F3>", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", opts)
-		vim.keymap.set("n", "<F4>", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
+		vim.keymap.set("n", "grd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
+		vim.keymap.set("n", "grD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
+		vim.keymap.set("n", "grs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
+		vim.keymap.set({ "n", "x" }, "grf", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", opts)
 	end,
 })
 
--- terminal/wezterm/wezterm.lua
+++ terminal/wezterm/wezterm.lua
@@ -1,111 +1,172 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local act = wezterm.action
 local mux = wezterm.mux
 
+local workspace_switcher = wezterm.plugin.require("https://github.com/MLFlexer/smart_workspace_switcher.wezterm")
+local resurrect = wezterm.plugin.require("https://github.com/MLFlexer/resurrect.wezterm")
+
 wezterm.on("gui-startup", function()
-  local tab, pane, window = mux.spawn_window{}
-  window:gui_window():maximize()
+	local tab, pane, window = mux.spawn_window({})
+	window:gui_window():maximize()
 end)
+
+-- loads the state whenever I create a new workspace
+wezterm.on("smart_workspace_switcher.workspace_switcher.created", function(window, path, label)
+  local workspace_state = resurrect.workspace_state
+
+  workspace_state.restore_workspace(resurrect.state_manager.load_state(label, "workspace"), {
+    window = window,
+    relative = true,
+    restore_text = true,
+    on_pane_restore = resurrect.tab_state.default_on_pane_restore,
+  })
+end)
+
+-- Saves the state whenever I select a workspace
+wezterm.on("smart_workspace_switcher.workspace_switcher.selected", function(window, path, label)
+  local workspace_state = resurrect.workspace_state
+  resurrect.state_manager.save_state(workspace_state.get_workspace_state())
+end)
+
+-- Resurrect on startup
+wezterm.on("gui-startup", resurrect.state_manager.resurrect_on_gui_startup)
 
 -- and finally, return the configuration to wezterm
 
 local keymaps = {
-    { key = "a", mods = "LEADER|CTRL",  action=wezterm.action{SendString="\x01"}},
-    { key = "-", mods = "LEADER",       action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}}},
-    { key = "|",mods = "LEADER",       action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}}},
-    { key = "s", mods = "LEADER",       action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}}},
-    { key = "v", mods = "LEADER",       action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}}},
-    { key = "c", mods = "LEADER",       action=wezterm.action{SpawnTab="CurrentPaneDomain"}},
-    { key = "h", mods = "LEADER",       action=wezterm.action{ActivatePaneDirection="Left"}},
-    { key = "j", mods = "LEADER",       action=wezterm.action{ActivatePaneDirection="Down"}},
-    { key = "k", mods = "LEADER",       action=wezterm.action{ActivatePaneDirection="Up"}},
-    { key = "l", mods = "LEADER",       action=wezterm.action{ActivatePaneDirection="Right"}},
-    { key = "H", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Left", 5}}},
-    { key = "J", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Down", 5}}},
-    { key = "K", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Up", 5}}},
-    { key = "L", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Right", 5}}},
-    { key = "1", mods = "LEADER",       action=wezterm.action{ActivateTab=0}},
-    { key = "2", mods = "LEADER",       action=wezterm.action{ActivateTab=1}},
-    { key = "3", mods = "LEADER",       action=wezterm.action{ActivateTab=2}},
-    { key = "4", mods = "LEADER",       action=wezterm.action{ActivateTab=3}},
-    { key = "5", mods = "LEADER",       action=wezterm.action{ActivateTab=4}},
-    { key = "6", mods = "LEADER",       action=wezterm.action{ActivateTab=5}},
-    { key = "7", mods = "LEADER",       action=wezterm.action{ActivateTab=6}},
-    { key = "8", mods = "LEADER",       action=wezterm.action{ActivateTab=7}},
-    { key = "9", mods = "LEADER",       action=wezterm.action{ActivateTab=8}},
-    { key = "&", mods = "LEADER|SHIFT", action=wezterm.action{CloseCurrentTab={confirm=true}}},
-    { key = "d", mods = "LEADER",       action=wezterm.action{CloseCurrentPane={confirm=true}}},
-    { key = "x", mods = "LEADER",       action=wezterm.action{CloseCurrentPane={confirm=true}}},
-    { key = "z", mods = 'LEADER',       action=wezterm.action.TogglePaneZoomState},
-    { key = "[", mods = 'LEADER',       action=wezterm.action.ActivateCopyMode},
-    { key = "n", mods = 'LEADER',       action=wezterm.action.ActivateTabRelative(1)},
-    { key = "p", mods = 'LEADER',       action=wezterm.action.ActivateTabRelative(-1)},
-    { key = "w", mods = 'LEADER',       action=wezterm.action.ShowTabNavigator},
-    { key = "a", mods = 'LEADER',       action=wezterm.action.ActivatePaneDirection('Next')},
-    -- { key = "a", mods = 'LEADER',       action=wezterm.action.PaneSelect},
-    {
-        key = ",",
-        mods = 'LEADER',
-        action = act.PromptInputLine {
-            description = 'Enter new name for tab',
-            action = wezterm.action_callback(
-                function(window, pane, line)
-                    if line then
-                        window:active_tab():set_title(line)
-                    end
-                end
-            ),
-        },
-    },
+	{ key = "a", mods = "LEADER|CTRL", action = wezterm.action({ SendString = "\x01" }) },
+	{ key = "-", mods = "LEADER", action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }) },
+	{ key = "|", mods = "LEADER", action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }) },
+	--{ key = "s", mods = "LEADER", action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }) },
+	{ key = "v", mods = "LEADER", action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }) },
+	{ key = "c", mods = "LEADER", action = wezterm.action({ SpawnTab = "CurrentPaneDomain" }) },
+	{ key = "h", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Left" }) },
+	{ key = "j", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Down" }) },
+	{ key = "k", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Up" }) },
+	{ key = "l", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Right" }) },
+	{ key = "H", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Left", 5 } }) },
+	{ key = "J", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Down", 5 } }) },
+	{ key = "K", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Up", 5 } }) },
+	{ key = "L", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Right", 5 } }) },
+	{ key = "1", mods = "LEADER", action = wezterm.action({ ActivateTab = 0 }) },
+	{ key = "2", mods = "LEADER", action = wezterm.action({ ActivateTab = 1 }) },
+	{ key = "3", mods = "LEADER", action = wezterm.action({ ActivateTab = 2 }) },
+	{ key = "4", mods = "LEADER", action = wezterm.action({ ActivateTab = 3 }) },
+	{ key = "5", mods = "LEADER", action = wezterm.action({ ActivateTab = 4 }) },
+	{ key = "6", mods = "LEADER", action = wezterm.action({ ActivateTab = 5 }) },
+	{ key = "7", mods = "LEADER", action = wezterm.action({ ActivateTab = 6 }) },
+	{ key = "8", mods = "LEADER", action = wezterm.action({ ActivateTab = 7 }) },
+	{ key = "9", mods = "LEADER", action = wezterm.action({ ActivateTab = 8 }) },
+	{ key = "&", mods = "LEADER|SHIFT", action = wezterm.action({ CloseCurrentTab = { confirm = true } }) },
+	{ key = "d", mods = "LEADER", action = wezterm.action({ CloseCurrentPane = { confirm = true } }) },
+	{ key = "x", mods = "LEADER", action = wezterm.action({ CloseCurrentPane = { confirm = true } }) },
+	{ key = "z", mods = "LEADER", action = wezterm.action.TogglePaneZoomState },
+	{ key = "[", mods = "LEADER", action = wezterm.action.ActivateCopyMode },
+	{ key = "n", mods = "LEADER", action = wezterm.action.ActivateTabRelative(1) },
+	{ key = "p", mods = "LEADER", action = wezterm.action.ActivateTabRelative(-1) },
+	{ key = "w", mods = "LEADER", action = wezterm.action.ShowTabNavigator },
+	{ key = "a", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Next") },
+	-- { key = "a", mods = 'LEADER',       action=wezterm.action.PaneSelect},
+	{
+		key = ",",
+		mods = "LEADER",
+		action = act.PromptInputLine({
+			description = "Enter new name for tab",
+			action = wezterm.action_callback(function(window, pane, line)
+				if line then
+					window:active_tab():set_title(line)
+				end
+			end),
+		}),
+	},
+	-- Switcher
+	{
+		key = "s",
+		mods = "LEADER",
+		action = workspace_switcher.switch_workspace(),
+	},
+	{
+		key = "S",
+		mods = "LEADER",
+		action = workspace_switcher.switch_to_prev_workspace(),
+	},
+	-- Resurrect
+	{
+		key = "w",
+		mods = "LEADER",
+		action = wezterm.action_callback(function(win, pane)
+			resurrect.state_manager.save_state(resurrect.workspace_state.get_workspace_state())
+		end),
+	},
+	{
+		key = "W",
+		mods = "LEADER",
+		action = wezterm.action_callback(function(win, pane)
+			resurrect.fuzzy_loader.fuzzy_load(win, pane, function(id, label)
+				local type = string.match(id, "^([^/]+)") -- match before '/'
+				id = string.match(id, "([^/]+)$") -- match after '/'
+				id = string.match(id, "(.+)%..+$") -- remove file extention
+				local opts = {
+					relative = true,
+					restore_text = true,
+					on_pane_restore = resurrect.tab_state.default_on_pane_restore,
+				}
+				local state = resurrect.state_manager.load_state(id, "workspace")
+				resurrect.workspace_state.restore_workspace(state, opts)
+			end)
+		end),
+	},
 }
 
 local colors = {
-  foreground = '#393736',
-  background = '#FDFCFB',
-  cursor_bg = '#2D2B2A',
-  cursor_fg = '#FDFCFB',
-  cursor_border = '#2D2B2A',
-  selection_fg = '#393736',
-  selection_bg = '#D4D1CF',
-  scrollbar_thumb = '#E9E7E5',
-  split = '#E2E0DE',
+	foreground = "#393736",
+	background = "#FDFCFB",
+	cursor_bg = "#2D2B2A",
+	cursor_fg = "#FDFCFB",
+	cursor_border = "#2D2B2A",
+	selection_fg = "#393736",
+	selection_bg = "#D4D1CF",
+	scrollbar_thumb = "#E9E7E5",
+	split = "#E2E0DE",
 
-  ansi = {
-    '#2D2B2A', -- black
-    '#D75F5F', -- red
-    '#5F875F', -- green
-    '#D7875F', -- yellow
-    '#5F87AF', -- blue
-    '#AF87AF', -- magenta
-    '#5E5C5A', -- cyan
-    '#393736', -- white
-  },
-  brights = {
-    '#5E5C5A', -- bright black
-    '#D75F5F', -- bright red
-    '#5F875F', -- bright green
-    '#D7875F', -- bright yellow
-    '#5F87AF', -- bright blue
-    '#AF87AF', -- bright magenta
-    '#5E5C5A', -- bright cyan
-    '#393736', -- bright white
-  },
+	ansi = {
+		"#2D2B2A", -- black
+		"#D75F5F", -- red
+		"#5F875F", -- green
+		"#D7875F", -- yellow
+		"#5F87AF", -- blue
+		"#AF87AF", -- magenta
+		"#5E5C5A", -- cyan
+		"#393736", -- white
+	},
+	brights = {
+		"#5E5C5A", -- bright black
+		"#D75F5F", -- bright red
+		"#5F875F", -- bright green
+		"#D7875F", -- bright yellow
+		"#5F87AF", -- bright blue
+		"#AF87AF", -- bright magenta
+		"#5E5C5A", -- bright cyan
+		"#393736", -- bright white
+	},
 }
 
 local config = {
-  font = wezterm.font_with_fallback({
-    "ComicShannsMono Nerd Font",
-  }),
+	font = wezterm.font_with_fallback({
+		"ComicShannsMono Nerd Font",
+	}),
 
-  font_size = 12.0, -- Ideal for the office
-  --font_size = 18.0, -- Ideal for the laptop
-  leader = { key="a", mods="CTRL" },
-  automatically_reload_config = true,
-  use_fancy_tab_bar = false,
-  hide_tab_bar_if_only_one_tab = true,
-  keys = keymaps,
-  colors = colors,
+	font_size = 12.0, -- Ideal for the office
+	--font_size = 18.0, -- Ideal for the laptop
+	leader = { key = "a", mods = "CTRL" },
+	automatically_reload_config = true,
+	use_fancy_tab_bar = false,
+	hide_tab_bar_if_only_one_tab = true,
+	keys = keymaps,
+	colors = colors,
 }
+
+workspace_switcher.apply_to_config(config)
 
 return config
